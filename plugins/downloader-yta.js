4be662b3ee69aa4dc8da7b0ecbe6345038d8a623
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-08-13 11:38:20 -0300

    Add files via upload

---
+++ plugins/downloader-yta.js
@@ -0,0 +1,39 @@
+let limit = 50
+import fs from 'fs'
+import fetch from 'node-fetch'
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
+let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
+if (!args || !args[0]) throw `*❗𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙡 𝙚𝙣𝙡𝙖𝙘𝙚 𝙙𝙚 𝙔𝙤𝙪𝙏𝙪𝙗𝙚 𝙥𝙖𝙧𝙖 𝙙𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧 𝙚𝙡 𝙖𝙪𝙙𝙞𝙤`
+conn.reply(m.chat, `*__⏳𝙀𝙨𝙥𝙚𝙧𝙚 𝙪𝙣𝙤𝙨 𝙨𝙚𝙜𝙪𝙣𝙙𝙤𝙨 𝙦𝙪𝙚 𝙚𝙣𝙫𝙞𝙖𝙢𝙤𝙨 𝙨𝙪𝙨 𝙫𝙞𝙙𝙚𝙤..⏳_*\n\n*◉ Sɪ Sᴜ ᴀᴜᴅɪᴏ ɴᴏ ᴇs ᴇɴᴠɪᴀᴅᴏ, ᴘʀᴜᴇʙᴇ ᴄᴏɴ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ #playdoc ᴏ #play.1 ᴏ #ytmp3doc ◉*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝚁𝙴𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝙾𝚁 𝙳𝙴 𝙰𝚄𝙳𝙸𝙾',
+body: '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `https://github.com/elrebelde21/The-LoliBot-MD`}}})
+let chat = global.db.data.chats[m.chat]
+const isY = /y(es)/gi.test(args[1])
+const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+const limitedSize = (isPrems || isOwner ? 350 : limit) * 3074
+let audio, source, res, link, lastError, isLimit
+for (let i in _audio) {
+try {
+audio = _audio[i]
+isLimit = limitedSize < audio.fileSizeH
+if (isLimit) continue
+link = await audio.download()
+if (link) res = await fetch(link)
+isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
+if (isLimit) continue
+if (res) source = await res.arrayBuffer()
+if (source instanceof ArrayBuffer) break
+} catch (e) {
+audio = link = source = null
+lastError = e
+}}
+if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw '*[❗] 𝙴𝚁𝚁𝙾𝚁: ' + (lastError || '𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝙰𝚄𝙳𝙸𝙾*')
+conn.sendFile(m.chat, source, title + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
+}
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
+handler.tags = ['downloader']
+handler.command = /^yt(a|mp3)$/i
+export default handler
