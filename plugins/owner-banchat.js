86215499cfbd8a0066c9b9155e2510e323c5a26f
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-08-15 14:13:49 -0300

    Delete plugins directory

--- plugins/owner-banchat.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = true
-m.reply('𝑬𝒔𝒕𝒆 𝒄𝒉𝒂𝒕 𝒇𝒖𝒆 𝒃𝒂𝒏𝒆𝒂𝒅𝒐🤑 𝑵𝒐 𝒆𝒔𝒕𝒂𝒓𝒆 𝒅𝒊𝒔𝒑𝒐𝒏𝒊𝒃𝒍𝒆 𝒉𝒂𝒔𝒕𝒂 𝒒𝒖𝒆 𝒔𝒆𝒂 𝒅𝒆𝒔𝒃𝒂𝒏𝒆𝒂𝒅𝒂')
-}
-handler.help = ['banchat']
-handler.tags = ['owner']
-handler.command = /^banchat$/i
-handler.rowner = true
-export default handler
--- plugins/owner-unbanchat.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = false
-m.reply('𝑬𝒔𝒕𝒆 𝒄𝒉𝒂𝒕 𝒇𝒖𝒆 𝒅𝒆𝒔𝒃𝒂𝒏𝒆𝒂𝒅𝒐🥳\n𝑨𝒉𝒐𝒓𝒂 𝒆𝒔𝒕𝒐𝒚 𝒅𝒊𝒔𝒑𝒐𝒏𝒊𝒃𝒍𝒆..')
-}
-handler.help = ['unbanchat']
-handler.tags = ['owner']
-handler.command = /^unbanchat$/i
-handler.rowner = true
-export default handler
